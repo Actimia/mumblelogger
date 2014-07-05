46ad16f96379df5937a174902a93553658675b35
Viking Edström <actimia1@gmail.com>
2014-07-05 18:33:50 +0200

    Compiles!

--- MumbleLogger.java
+++ MumbleLogger.java
@@ -2,16 +2,27 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.*;
 import java.util.regex.*;
 
 import java.net.URL;
+import java.net.MalformedURLException;
+
+import java.nio.charset.Charset;
 
 import java.sql.*;
 
-import com.google.gson.*
+import com.google.gson.*;
 
 import org.apache.http.*;
+import org.apache.http.client.*;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.client.methods.*;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 
 public class MumbleLogger {
     // a rudimentary url pattern
@@ -24,8 +35,8 @@ public class MumbleLogger {
     private static final String CLIENT_ID = "b8cdd34d59c9b9f";
 
     // client to use for api requests
-    private static final HttpClient http = new HttpClientBuilder()
-        .setConnectionManager(new MultiThreadedHttpConnectionManager())
+    private static final HttpClient http = HttpClientBuilder.create()
+        .setConnectionManager(new PoolingHttpClientConnectionManager())
         .build();
     private static final JsonParser jsonparser = new JsonParser();
 
@@ -95,59 +106,70 @@ public class MumbleLogger {
             return Stream.of(url);
         } else if (url.getHost().equals("imgur.com") && url.getPath().startsWith("/a/")) {
             // album case, fetch urls of all the images in the album and add them separately
-            String albumToken = url.getPath().substring(3)); // discard the /a/
+            String albumToken = url.getPath().substring(3); // discard the /a/
             HttpGet req = new HttpGet("https://api.imgur.com/3/gallery/album/" + albumToken);
 
             // add auth header
             req.addHeader("Authorization", "Client-ID " + CLIENT_ID);
+            try {
+                System.out.println("Resolving album: " + albumToken);
+                HttpResponse res = http.execute(req);
+                if(res.getStatusLine().getStatusCode() == 200){
+                    // get the response, extract the image array
+                   
+                    String json = EntityUtils.toString(res.getEntity());
+                    JsonObject root = jsonparser.parse(json).getAsJsonObject();
+                    JsonArray images = root.getAsJsonArray("images");
 
-            System.out.println("Resolving album: " + albumToken);
-            HttpResponse res = http.execute(req);
-            if(res.getStatusLine().getStatusCode() == 200){
-                // get the response, extract the image array
-                String json = EntityUtils.toString(res.getEntity());
-                JsonObject root = jsonparser.parse(json).getAsJsonObject();
-                JsonArray images = root.getAsJsonArray("images");
-
-                // get all the image urls
-                ArrayList<URL> urls = new ArrayList<>();
-                for(int i = 0; i < images.size(); i++){
-                    JsonObject image = images.get(i).getAsJsonObject();
-                    String url = image.get("link").getAsString();
-                    urls.add(new URL(url));
+                    // get all the image urls
+                    ArrayList<URL> urls = new ArrayList<>();
+                    for(int i = 0; i < images.size(); i++){
+                        JsonObject image = images.get(i).getAsJsonObject();
+                        String link = image.get("link").getAsString();
+                        urls.add(new URL(link));          
+                    }
+                    return urls.stream();
+                } else {
+                    // something went wrong
+                    System.out.println("Non-200 statuscode from album info fetch: " + res.getStatusLine().toString());
+                    return Stream.empty();
                 }
-                return urls.stream();
-            } else {
-                // something went wrong
-                System.out.println("Non-200 statuscode from album info fetch: " + res.getStatusLine().toString());
+            } catch (Exception e) {
+                e.printStackTrace();
                 return Stream.empty();
             }
+                
+            
         } else if (isImageUrl(url)) {
             // image hosted elsewhere on the web, rehost to imgur
-            HttpPost req = new HttpPost("https://api.imgur.com/3/image/");
+            try {
+                HttpPost req = new HttpPost("https://api.imgur.com/3/image/");
+                // add auth header
+                req.addHeader("Authorization", "Client-ID " + CLIENT_ID);
 
-            // add auth header
-            req.addHeader("Authorization", "Client-ID " + CLIENT_ID);
+                // add the image path to the request
+                List<NameValuePair> nvps = new ArrayList<>();
+                nvps.add(new BasicNameValuePair("image", url.toString()));
+                req.setEntity(new UrlEncodedFormEntity(nvps, Charset.forName("UTF-8")));
 
-            // add the image path to the request
-            List<NameValuePair> nvps = new ArrayList<>();
-            nvps.add(new BasicNameValuePair("image", url.toString()));
-            req.setEntity(new UrlEncodedFormEntity(nvps, HTTP.UTF_8));
-
-            System.out.println("Uploading image from: " + url.getHost());
-            HttpResponse res = http.execute(req);
-            if(res.getStatusLine().getStatusCode() == 200){
-                // get the response, extract the image link
-                String json = EntityUtils.toString(res.getEntity());
-                JsonObject root = jsonparser.parse(json).getAsJsonObject();
-                JsonObject image = root.get("data").getAsJsonObject();
-                String url = image.get("link").getAsString();
-                return Stream.of(new URL(url));
-            } else {
-                // something went wrong
-                System.out.println("Non-200 statuscode from image upload: " + res.getStatusLine().toString());
+                System.out.println("Uploading image from: " + url.getHost());
+                HttpResponse res = http.execute(req);
+                if(res.getStatusLine().getStatusCode() == 200){
+                    // get the response, extract the image link
+                    String json = EntityUtils.toString(res.getEntity());
+                    JsonObject root = jsonparser.parse(json).getAsJsonObject();
+                    JsonObject image = root.get("data").getAsJsonObject();
+                    String link = image.get("link").getAsString();
+                    return Stream.of(new URL(link));
+                } else {
+                    // something went wrong
+                    System.out.println("Non-200 statuscode from image upload: " + res.getStatusLine().toString());
+                    return Stream.empty();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
                 return Stream.empty();
-            }
+            }  
         } else {
             // not recognised as an image
             return Stream.empty();
